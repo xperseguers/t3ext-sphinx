43d532a1c331fadc37bdc683f0fb34c915afd726
Xavier Perseguers <xavier@causal.ch>
2018-10-27 11:40:20 +0200

    [TASK] Raise version to 2.5.2-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,10 +29,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '2.5.1',
+    'version' => '2.5.2-dev',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.1.99',
+            'php' => '5.5.0-7.2.99',
             'typo3' => '7.6.0-8.7.99',
         ],
         'conflicts' => [],
